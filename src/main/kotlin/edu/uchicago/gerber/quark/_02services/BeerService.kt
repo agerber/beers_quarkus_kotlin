333f6dc89248ff915429aaa76ee733db9c9b8194
Adam Gerber <adam.gerber@yahoo.com>
2023-07-12 19:59:37 -0500

    add intermediate service layer

---
+++ src/main/kotlin/edu/uchicago/gerber/quark/_02services/BeerService.kt
@@ -0,0 +1,52 @@
+package edu.uchicago.gerber.quarkus._02services
+
+
+
+import com.fasterxml.jackson.databind.BeanDescription
+import edu.uchicago.gerber.quarkus._03repositories.MongoBeerRepository
+import edu.uchicago.gerber.quarkus._04models.Beer
+import io.quarkus.mongodb.panache.kotlin.PanacheQuery
+import jakarta.annotation.PostConstruct
+import jakarta.annotation.PreDestroy
+import jakarta.enterprise.context.ApplicationScoped
+import jakarta.inject.Inject
+
+@ApplicationScoped
+class BeerService  {
+
+    @Inject
+    lateinit var concreteRepository: MongoBeerRepository
+
+
+    fun create(beer: Beer){
+        concreteRepository._create(beer)
+    }
+    fun create(beers: List<Beer>){
+        concreteRepository._create(beers)
+    }
+    fun readById(id:String): Beer{
+      return  concreteRepository._readById(id)
+    }
+    fun readAll(): List<Beer>{
+        return  concreteRepository._readAll()
+    }
+    fun update(updatedBeer: Beer): Beer{
+        return  concreteRepository._update(updatedBeer)
+    }
+    fun deleteById(id:String): Beer{
+        return  concreteRepository._deleteById(id)
+    }
+    fun deleteAll(){
+        concreteRepository._deleteAll()
+    }
+    fun count() : Long{
+       return concreteRepository._count()
+    }
+    fun findAll(): PanacheQuery<Beer>?{
+        return concreteRepository._findAll()
+
+    }
+
+
+
+}
