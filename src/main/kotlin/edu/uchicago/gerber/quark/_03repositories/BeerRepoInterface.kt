3e5661274e908a13e51ba90d3d8cda82338ad2a2
Adam Gerber <adam.gerber@yahoo.com>
2023-07-12 20:26:34 -0500

    add beer repo interface

---
+++ src/main/kotlin/edu/uchicago/gerber/quark/_03repositories/BeerRepoInterface.kt
@@ -0,0 +1,20 @@
+package edu.uchicago.gerber.quark._03repositories
+
+
+import edu.uchicago.gerber.quarkus._04models.Beer
+import io.quarkus.mongodb.panache.kotlin.PanacheQuery
+import org.bson.types.ObjectId
+
+interface BeerRepoInterface {
+
+    fun _create(beer: Beer)
+    fun _create(beers: List<Beer>)
+    fun _readById(id:String): Beer
+    fun _readAll(): List<Beer>
+    fun _update(updatedBeer: Beer)
+    fun _deleteById(id:String)
+    fun _deleteAll()
+    fun _count() : Long
+    fun _findAll(): PanacheQuery<Beer>?
+
+}
