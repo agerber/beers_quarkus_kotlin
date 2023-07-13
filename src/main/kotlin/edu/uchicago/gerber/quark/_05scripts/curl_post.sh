951acd01484af97ff6a842dc347e36815527f301
Adam Gerber <adam.gerber@yahoo.com>
2023-07-12 20:06:12 -0500

    add scripts

---
+++ src/main/kotlin/edu/uchicago/gerber/quark/_05scripts/curl_post.sh
@@ -0,0 +1,7 @@
+curl -X POST localhost:8080/beers/ \
+   -H 'Content-Type: application/json' \
+   -d '{"name":"TestName", "hop":"TestHop", "malt":"TestMalt", "style":"TestStyle",
+   "yeast":"TestYeast"}'
+
+
+#for postman version: copy into postman || import || raw-text
---
+++ src/main/kotlin/edu/uchicago/gerber/quark/_05scripts/curl_put.sh
@@ -0,0 +1,7 @@
+curl -X PUT localhost:8080/beers/ \
+   -H 'Content-Type: application/json' \
+   -d '{"id":"5063114bd386d8fadbd6b004", "name":"TestNameU", "hop":"TestHopU", "malt":"TestMaltU", "style":"TestStyleU",
+   "yeast":"TestYeastU"}'
+
+
+#for postman version: copy into postman || import || raw-text
